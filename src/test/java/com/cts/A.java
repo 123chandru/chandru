44a615b41b8028ac919c9fe44dfca0f4a805ea5a
123chandru <chandrusaravanan1996@gmail.com>
2022-11-09 12:11:46 +0530

    program done

--- src/test/java/com/cts/A.java
+++ src/test/java/com/cts/A.java
@@ -1,5 +1,17 @@
 package com.cts;
 
+import java.sql.Driver;
+
 public class A {
+	public void abc() {
+		System.out.println("aaaa");
+
+	}
+	public static void main(String[] args) {
+		
+		A c = new A();
+		c.abc();
+		
+	}
 
 }
